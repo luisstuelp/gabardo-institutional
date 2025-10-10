974b13f485df1e45e7f2130eafa4bf0b8d765824
David Philien <davidplgomes@gmail.com>
2025-10-09 23:19:46 -0300

    fix: Sustentabilidade tabs

--- src/components/custom/NewSustainabilityHeroSection.tsx
+++ src/components/custom/NewSustainabilityHeroSection.tsx
@@ -46,7 +46,7 @@ export default function NewSustainabilityHeroSection({ activeTab, onTabClick }: 
               type="button"
               key={tab}
               onClick={() => onTabClick(tab)}
-              className={`px-6 py-3 text-sm font-medium transition-all duration-300 relative 
+              className={`px-3 lg:px-6 py-2 text-xs lg:text-sm font-medium transition-all duration-300 relative 
                 ${activeTab === tab ? 'text-white' : 'text-gray-300 hover:text-white'}`
               }
             >
